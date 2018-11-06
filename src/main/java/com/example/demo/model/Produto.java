e8386e9bc522df25f9aadda5bc2a02401eb5bf6d
flavioriper <flavioriper_andrade@hotmail.com>
2018-11-06 20:08:16 -0200

    initial commit

---
+++ src/main/java/com/example/demo/model/Produto.java
@@ -0,0 +1,72 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	private String nome;
+	
+	private BigDecimal valor;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/resource/ProdutosResource.java
@@ -0,0 +1,27 @@
+package com.example.demo.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Produto;
+import com.example.demo.repository.Produtos;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/produtos")
+public class ProdutosResource {
+	
+	@Autowired
+	private Produtos produtos;
+	
+	@GetMapping
+	public List<Produto> listar(){
+		return produtos.findAll();
+	}
+
+}
